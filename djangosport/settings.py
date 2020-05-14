b8a8741112d8b7edac730b06a6bc4c0bb35e1eb3
turingmachiine <strvtxv@gmail.com>
2020-05-14 17:25:17 +0300

    deploy

--- djangosport/settings.py
+++ djangosport/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = s.path.join(BASE_DIR, 'static'),
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
